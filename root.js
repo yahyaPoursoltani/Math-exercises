732ac30c0e5ba0cba6ea3f0babfe2e9dd4bec7f2
Yahya Poursoltani <yahya.poursoltani@gmail.com>
2015-10-01 22:41:03 +0330

    root.js v 1.1

--- root.js
+++ root.js
@@ -1,23 +1,21 @@
-//Powered by Yahya Poursoltani @ 9/7/1394 at 15:46 PM
-//get suprimum & infimum
-var sup = 10 ;
-var inf = -10 ;
-var roots = (sup + inf)/2;
-var eps = 0.001;
+//Author: Yahya Poursoltani @ 9/7/1394 at 15:46 PM
+
 //difine Your own function
 var f = function(x){
 	return x - 0.5*Math.cos(x);
-};
+}; //End Function
+var sup = 10 ;   //Highest Boundry
+var inf = -10 ;  //Lowest Boundry
+var eps = 0.001; //Result Precision
 
- while(Math.abs(f(roots))>eps ){
+// *********** Program Starts Here **************
+while(Math.abs(f(roots = (sup + inf)/2))>eps ){
 	if(f(sup)*f(roots)>0){
 		sup=roots;
 	}else{
 		inf=roots;
 	};
 	roots = (sup + inf)/2;
- };
+};
 console.log("Result : ", roots);
 
-
-//end*****
