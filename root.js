b64f14a72418caa1d992cecf1c7c64f236b2329d
Yahya Poursoltani <yahya.poursoltani@gmail.com>
2015-10-02 13:59:50 +0330

    first commit

--- root.js
+++ root.js
@@ -1,21 +1,25 @@
 //Author: Yahya Poursoltani @ 9/7/1394 at 15:46 PM
 
-//difine Your own function
-var f = function(x){
-	return x - 0.5*Math.cos(x);
-}; //End Function
-var sup = 10 ;   //Highest Boundry
-var inf = -10 ;  //Lowest Boundry
-var eps = 0.001; //Result Precision
 
-// *********** Program Starts Here **************
-while(Math.abs(f(roots = (sup + inf)/2))>eps ){
-	if(f(sup)*f(roots)>0){
-		sup=roots;
-	}else{
-		inf=roots;
+//Binary Equation Solver Constructor
+var BinaryEqSolver = function(func, maxi, mini, precision){
+	this.sup = maxi;      //Highest Boundry
+	this.inf = mini;      //Lowest Boundry
+	this.eps = precision; //Result Precision
+	this.f=func;          //Function To Be Solved
+}
+//Add Binary Equation Solver Methods to its Prototype
+BinaryEqSolver.prototype.result = function(){
+	var root;
+	while(Math.abs(this.f(root = (this.sup + this.inf)/2))>this.eps ){      
+		if(this.f(this.sup)*this.f(root)>0) this.sup=root
+		else this.inf=root;
 	};
-	roots = (sup + inf)/2;
+	return root;
 };
-console.log("Result : ", roots);
+
+
+//Program starts here
+var test = new BinaryEqSolver(function(x){return x+1}, 10, -10, 0.001); 
+console.log(test.result());
 
